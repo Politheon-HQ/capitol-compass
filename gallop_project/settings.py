054531c3740ae3a68ca1d048475b62b2732b67f6
Bodnar, Hanna <htuple3@gatech.edu>
2025-03-15 12:53:04 -0400

    Fix database connection for Heroku MySQL

--- gallop_project/settings.py
+++ gallop_project/settings.py
@@ -81,9 +81,8 @@ WSGI_APPLICATION = "gallop_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 DATABASES = {
-    "default": dj_database_url.config(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"), conn_max_age=600, ssl_require=True)
 }
 
 #DATABASES = {
