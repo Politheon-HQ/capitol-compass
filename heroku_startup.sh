6691fdc065e4ec804544278d4e53515c7dc7716f
Bodnar, Hanna <htuple3@gatech.edu>
2025-03-15 21:01:32 -0400

    Prevent CA certificate from being overwritten on restart

--- heroku_startup.sh
+++ heroku_startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 mkdir -p /app/gallop_project
-echo "$DB_SSL_CA" | tr ' ' '\n'  > /app/gallop_project/ca-certificate.crt
-echo "CA Certificate written to /app/gallop_project/ca-certificate.crt"
+
+if [ ! -f "/app/gallop_project/ca-certificate.crt" ]; then
+    echo "$DB_SSL_CA" | tr ' ' '\n' > /app/gallop_project/ca-certificate.crt
+    echo "CA certificate written to /app/gallop_project/ca-certificate.crt"
+else
+    echo "CA certificate already exists."
+fi
 
 exec gunicorn gallop_project.wsgi --log-file -
