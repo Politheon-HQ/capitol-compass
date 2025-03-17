3d1680bb2d0f4cbae96e8fb95a497b60a5d5e2b0
Bodnar, Hanna <htuple3@gatech.edu>
2025-03-17 00:30:22 -0400

    Fix Redis SSL connection issue.

--- gallop_project/settings.py
+++ gallop_project/settings.py
@@ -92,7 +92,7 @@ CACHES = {
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'CONNECTION_POOL_KWARGS': {
-                'max_connections': 100,
+                'ssl_cert_reqs': None,  # Disable SSL certificate verification
             },
         }
     }
